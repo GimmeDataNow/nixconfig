f6b03e04a44d55ddb7dec07024dc94b67cf00bbd
GimmeDataNow <alan.omazic@gmail.com>
2024-09-20 19:21:53 +0200

    testing 6

--- nixos/min-configuration.nix
+++ nixos/min-configuration.nix
@@ -32,8 +32,8 @@ in
   boot.loader.systemd-boot.enable = true; # use systemd-boot
   boot.loader.efi.canTouchEfiVariables = true; # avoid potential issues with efi
   # boot.kernelPackages = pkgs.linuxPackages_latest; # newest nixos linux kernel version != lastest kernel version
-  # boot.kernelPackages = pkgs.linuxPackages_6_9; # newest nixos linux kernel version != lastest kernel version
-  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest; # https://discourse.nixos.org/t/unable-to-build-nix-due-to-nvidia-drivers-due-or-kernel-6-10/49266/17
+  boot.kernelPackages = pkgs.linuxPackages_6_9; # newest nixos linux kernel version != lastest kernel version
+  # boot.kernelPackages = pkgs.linuxPackages_xanmod_latest; # https://discourse.nixos.org/t/unable-to-build-nix-due-to-nvidia-drivers-due-or-kernel-6-10/49266/17
   boot.kernelParams = [ 
     # "initcall_blacklist=simpledrm_platform_driver_init" # https://github.com/hyprwm/Hyprland/issues/6967#issuecomment-2241948730
     "nvidia.NVreg_PreserveVideoMemoryAllocations=1" # https://wiki.hyprland.org/Nvidia/#suspendwakeup-issues
