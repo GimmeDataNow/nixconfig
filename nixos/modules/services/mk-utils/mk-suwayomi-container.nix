d503c149f95827b3439742d07ec83dfa4324424b
GimmeDataNow <alan.omazic@gmail.com>
2025-06-22 21:00:27 +0200

    attempt 5 with updated container logic

--- nixos/modules/services/mk-utils/mk-suwayomi-container.nix
+++ nixos/modules/services/mk-utils/mk-suwayomi-container.nix
@@ -5,12 +5,14 @@ name: port: index:
   containers = {
     "${name}" = {
       autoStart = true;
-      privateNetwork = true;
-      hostAddress = "192.168.0.110";
-      localAddress = "192.168.0.${toString (10 + index)}";
+
+      useHostNetwork = true;
+      
+      # privateNetwork = true;
+      # hostAddress = "192.168.0.110";
+      # localAddress = "192.168.0.${toString (10 + index)}";
 
       config = { config, pkgs, ... }: {
-        networking.useHostNetwork = true;
 
         services.suwayomi-server = {
           enable = true;
