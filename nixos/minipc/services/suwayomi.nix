15244935ab612f15813db7690f2cbb4c00067f86
GimmeDataNow <alan.omazic@gmail.com>
2025-06-22 20:25:17 +0200

    attempting to switch to nixos-containers

--- nixos/minipc/services/suwayomi.nix
+++ nixos/minipc/services/suwayomi.nix
@@ -1,10 +1,21 @@
-{
-  services.suwayomi-server = {
-    enable = true;
-    openFirewall = true;
-    settings.server = {
-      port = 4567;
-      extensionRepos = ["https://raw.githubusercontent.com/keiyoushi/extensions/repo/index.min.json"];
-    };
-  };
+{ config, pkgs, lib, inputs, ... }:
+
+let
+# ../services/mk-utils/mk-suwayomi-container.nix
+  mkSuwayomiContainer = import ../services/mk-utils/mk-suwayomi-container.nix { inherit lib; };
+
+  containerSpecs = [
+    # (mkSuwayomiContainer "suwayomi1" 4567)
+    (mkSuwayomiContainer "suwayomi2" 4568)
+    (mkSuwayomiContainer "suwayomi3" 4569)
+  ];
+
+  # Apply index during mapping
+  containerDefs = lib.imap0 (index: containerFunc: containerFunc index) containerSpecs;
+
+in {
+  config = lib.mkMerge containerDefs;
 }
+
+
+
