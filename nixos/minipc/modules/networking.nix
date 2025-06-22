65a347473d70921d0295262312815623c2c30c7f
GimmeDataNow <alan.omazic@gmail.com>
2025-06-22 22:53:10 +0200

    fix the systemd-resolved bug

--- nixos/minipc/modules/networking.nix
+++ nixos/minipc/modules/networking.nix
@@ -3,5 +3,4 @@
   networking.hostName = "minipc"; # hostname
   networking.networkmanager.enable = true; # use networkmanager
   networking.firewall.allowedTCPPorts = [22 53317]; # 53317 is used by local-send
-  services.resolved.enable = true;
 }
