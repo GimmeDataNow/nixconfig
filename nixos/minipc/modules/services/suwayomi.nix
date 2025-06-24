b356fedffd88e7ddf80b3d205781631565f1fc8c
GimmeDataNow <alan.omazic@gmail.com>
2025-06-24 16:52:19 +0200

    fixing yazi plugin, minor changes to formatting in ~/nixos/

--- nixos/minipc/modules/services/suwayomi.nix
+++ nixos/minipc/modules/services/suwayomi.nix
@@ -1,25 +1,12 @@
-# /etc/nixos/modules/services/suwayomi.nix
-#
-# This module configures the suwayomi-server service inside a container.
-
 { config, lib, pkgs, containerDef, ... }:
 
-# `specialArgs` from the factory passes our container definition here.
-# { containerDef, ... }:
-
 {
-  # Configure the suwayomi-server service.
   services.suwayomi-server = {
     enable = true;
     openFirewall = false; # The host manages the firewall.
     settings.server = {
-      # Use the port that was assigned to this container in the main config list.
       port = containerDef.hostPort;
-      # The rest of your settings.
       extensionRepos = ["https://raw.githubusercontent.com/keiyoushi/extensions/repo/index.min.json"];
     };
   };
-
-  # By enabling the service, NixOS automatically creates the `suwayomi-server` user,
-  # satisfying the requirement to run as the intended user.
 }
--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -4,4 +4,5 @@
   networking.networkmanager.enable = true; # use networkmanager
   networking.firewall.allowedTCPPorts = [53317]; # 53317 is used by local-send
   services.resolved.enable = true;
+  networking.enableIPv6 = false;
 }
