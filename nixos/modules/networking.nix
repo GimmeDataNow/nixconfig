d0fc77dab89e1bfa81ae292fc7aff89f35404385
GimmeDataNow <alan.omazic@gmail.com>
2025-07-01 16:28:58 +0200

    attempting to fix the constant network issues

--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -1,8 +1,25 @@
 { ... }: {
  # networking & security
   networking.hostName = "mainpc"; # hostname
-  networking.networkmanager.enable = true; # use networkmanager
   networking.firewall.allowedTCPPorts = [53317]; # 53317 is used by local-send
-  services.resolved.enable = true;
   networking.enableIPv6 = false;
+
+  networking.networkmanager.enable = true; # use networkmanager
+
+  # Let resolv.conf be managed automatically
+  networking.useHostResolvConf = false;
+  environment.etc."resolv.conf".source = "/run/systemd/resolve/stub-resolv.conf";
+
+  # Enable systemd-resolved
+  services.resolved = {
+    enable = true;
+    dnssec = "false";           # optional
+    dnsovertls = "false";       # optional
+    domains = [];             # your search domains, if any
+    fallbackDns = [ "1.1.1.1" "1.0.0.1" ];         # fallback if DHCP fails
+    extraConfig = ''
+      # Ensure stub resolver is enabled
+      DNSStubListener=yes
+    '';
+  };
 }
