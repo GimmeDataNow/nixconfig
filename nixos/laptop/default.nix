da22a8a4425693e08559d58b4851b95fccc3857f
GimmeDataNow <alan.omazic@gmail.com>
2025-07-03 18:43:06 +0200

    moved the laptop config to a module based config

---
+++ nixos/laptop/default.nix
@@ -0,0 +1,32 @@
+{ ... }: {
+
+  imports = [
+    # essential modules
+    ./modules/hardware.nix
+    ./modules/boot.nix
+    ./modules/users.nix
+    ./modules/time_locale.nix
+    ./modules/networking.nix
+    ./modules/security.nix
+    ./modules/sound.nix
+    ./modules/fonts.nix
+    ./modules/environment.nix
+    ./modules/printer.nix
+    ./modules/desktop.nix
+
+    # optional modules
+    ./modules/programs.nix
+    ./modules/spicetify.nix
+
+  ];
+  
+  # Base system config
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowInsecure = true;
+    allowBroken = true;
+  };
+
+  system.stateVersion = "25.05";
+}
---
+++ nixos/laptop/modules/printer.nix
@@ -0,0 +1,1 @@
+../../modules/printer.nix
