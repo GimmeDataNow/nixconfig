77798d9c1dba810a004e9fcaceec4c77ad2a0c56
GimmeDataNow <alan.omazic@gmail.com>
2026-02-22 12:07:21 +0100

    update the desktop config

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,20 +1,49 @@
-{ pkgs, unstable, inputs, user, ... }: {
+{ pkgs, unstable, inputs, lib, user, ... }: {
   imports = [
     # 1. Physical Hardware scan (Specific to this machine)
     ./hardware.nix
 
     # 2. Reusable NixOS Modules (We will create these next)
+    ../../modules/nixos/common/boot.nix
     ../../modules/nixos/common/common.nix      # Core settings for all machines
-    ../../modules/nixos/desktop/gui.nix # Specific to GUI machines (Nvidia, Audio)
+    ../../modules/nixos/common/location.nix    # Allows for the time to automatically update based on location
+    ../../modules/nixos/desktop/networking.nix
+    ../../modules/nixos/desktop/gpu-amd.nix
+    ../../modules/nixos/desktop/hyprland.nix
+    ../../modules/nixos/desktop/fonts.nix
+    ../../modules/nixos/desktop/programs/cli.nix
+    ../../modules/nixos/desktop/programs/communication.nix
+    ../../modules/nixos/desktop/programs/gaming.nix
+    ../../modules/nixos/desktop/programs/gui.nix
     
     # 3. Home Manager Integration (The "Dotfiles" engine)
     inputs.home-manager.nixosModules.home-manager {
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
       home-manager.extraSpecialArgs = { inherit inputs unstable user; };
-      home-manager.users.${user} = import ../../modules/home/desktop.nix;
+      # home-manager.users.${user} = import ../../modules/home/desktop.nix;
+      home-manager.backupFileExtension = "bak";
+      home-manager.users.${user} = {
+        imports = [
+          ../../modules/home/hyprland/default.nix
+          ../../modules/home/desktop.nix
+          ../../modules/home/shell.nix
+          ../../modules/home/wayland.nix
+          ../../modules/home/spicetify.nix
+        ];
+
+        wayland.windowManager.hyprland.settings = {
+          monitor = [
+            ", preferred, auto, 1"
+          ];
+        };
+      };
+
     }
   ];
+
+  # This override ensures geoclue takes precedence over the "UTC" default
+  time.timeZone = lib.mkForce null;
 
   # Autologin
   services.getty.autologinUser = "${user}";
