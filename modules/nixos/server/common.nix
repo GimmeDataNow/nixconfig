9a3421685e4b7c848bf33db722f3f45ea2a82899
GimmeDataNow <alan.omazic@gmail.com>
2026-02-23 08:46:23 +0100

    ssh attempt 2

--- modules/nixos/server/common.nix
+++ modules/nixos/server/common.nix
@@ -1,37 +1,54 @@
 { pkgs, lib, ... }: {
   # --- SECURITY ---
+  # services.openssh = {
+  #   enable = true;
+  #   settings = {
+  #     PasswordAuthentication = false;
+  #     KbdInteractiveAuthentication = false;
+  #     PermitRootLogin = "prohibit-password";
+  #   };
+
+  #   # Space-separated list. Ensure there are no commas.
+  #   extraConfig = ''
+  #     Match Address 137.226.218.185,127.0.0.1,192.168.0.0/24,100.64.0.0/10
+  #         PasswordAuthentication yes
+  #         KbdInteractiveAuthentication yes
+  #   '';
+  # };
   services.openssh = {
     enable = true;
     settings = {
+      # 1. Global: Only keys allowed
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
       PermitRootLogin = "prohibit-password";
     };
 
-    # Space-separated list. Ensure there are no commas.
-    extraConfig = ''
-      Match Address 137.226.218.185,127.0.0.1,192.168.0.0/24,100.64.0.0/10
-          PasswordAuthentication yes
-          KbdInteractiveAuthentication yes
+    # 2. Use lib.mkOrder to ensure this is at the VERY end of the file.
+    # We use wildcards (*) because they are the most compatible with sshd's Match Address.
+    extraConfig = lib.mkOrder 1000 ''
+      Match Address 137.226.218.185 127.0.0.1 192.168.0.* 100.*
+        PasswordAuthentication yes
+        KbdInteractiveAuthentication yes
     '';
   };
   
   # Fail2Ban: Bans IPs that try to brute force your SSH
-  services.fail2ban = {
-    enable = true;
-    maxretry = 5;
-    ignoreIP = [
-      "137.226.218.185"
+  # services.fail2ban = {
+  #   enable = true;
+  #   maxretry = 5;
+  #   ignoreIP = [
+  #     "137.226.218.185"
 
-      # Loopback
-      "127.0.0.1/8"
+  #     # Loopback
+  #     "127.0.0.1/8"
 
-      # LAN
-      "192.168.0.0/24"
-      # Tailscale CGNAT range
-      "100.64.0.0/10"
-    ];
-  };
+  #     # LAN
+  #     "192.168.0.0/24"
+  #     # Tailscale CGNAT range
+  #     "100.64.0.0/10"
+  #   ];
+  # };
 
   # --- NETWORKING & FIREWALL ---
   networking.firewall = {
