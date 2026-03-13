c96538b0bcd5d9a47e9b7e48bbe5f1ec3f138ac0
GimmeDataNow <alan.omazic@gmail.com>
2026-03-13 20:13:35 +0000

    added vaultwarden

--- hosts/minipc/configuration.nix
+++ hosts/minipc/configuration.nix
@@ -13,6 +13,7 @@
     ../../modules/nixos/server/suwayomi/compose.nix
     ../../modules/nixos/server/mealie/compose.nix
     ../../modules/nixos/server/technitium/compose.nix
+    ../../modules/nixos/server/vaultwarden.nix
     # Home Manager (Just for Shell/CLI tools)
     inputs.home-manager.nixosModules.home-manager
   ];
---
+++ modules/nixos/server/vaultwarden.nix
@@ -0,0 +1,24 @@
+{
+  # services.tailscale.enable = true;
+  services.vaultwarden = {
+    enable = true;
+    config = {
+      DOMAIN = "https://minipc.tail554692.ts.net";
+      ROCKET_ADDRESS = "127.0.0.1";
+      ROCKET_PORT = 8222;
+    };
+  };
+
+  # services.caddy = {
+  #   enable = true;
+  #   virtualHosts."minipc.tail554692.ts.net".extraConfig = ''
+  #     reverse_proxy localhost:8222
+  #   '';
+  # };
+
+  # networking.firewall.allowedTCPPorts = [ 80 443 ];
+  # networking.firewall.trustedInterfaces = [ "tailscale0" ];
+
+  # systemd.services.caddy.serviceConfig.SupplementaryGroups = [ "tailscale" ];
+
+}
